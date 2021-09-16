bb6f62a2eb710d3f96e04c7275d03d6e9edbc629
Megha Joshi <33493716+meghapsimatrix@users.noreply.github.com>
2021-09-16 15:04:58 -0500

    Add files via upload

---
+++ example/example_dat_clean.R
@@ -0,0 +1,81 @@
+library(tidyverse)
+
+
+dat <- read_csv("example/cyber_bullying_clean.csv")
+
+
+dat <- dat %>%
+  rename(assignment = He_asgn_type,
+         outcome = Co02_macro.1,
+         school_setting = Sa22_scl_type)
+
+
+dat <- dat %>%
+  rename(es = yi_flp,
+         var = vi,
+         study_id = StudyID,
+         factor_1 = assignment,
+         factor_2 = outcome, 
+         factor_3 = school_setting) %>%
+  mutate(factor_1 = case_when(factor_1 == "non_cls" ~ "Non-Randomized Class",
+                              factor_1 == "non_ind" ~ "Non-Randomized Individual",
+                              factor_1 == "non_scl" ~ "Non-Randomized School",
+                              factor_1 == "ran_cls" ~ "Randomized Class",
+                              factor_1 == "ran_ind" ~ "Randomized Individual",
+                              factor_1 == "ran_scl" ~ "Randomized School"),
+         factor_2 = str_sub(factor_2, 5),
+         factor_3 = str_sub(factor_3, 4))
+
+
+
+dat_sum_3 <-
+  dat %>%
+  group_by(factor_1, factor_2, factor_3) %>%
+  group_modify(~ tidy_meta(.x)) %>%
+  ungroup()
+
+# dat_sum_3 <-
+#   dat_sum_3 %>%
+#   rename(assignment = factor_1,
+#          outcome = factor_2,
+#          school_setting = factor_3)
+
+write_csv(dat_sum_3, "example/dat_sum_3.csv")
+
+dat_sum_2 <-
+  dat %>%
+  group_by(factor_1, factor_2) %>%
+  group_modify(~ tidy_meta(.x)) %>%
+  ungroup()
+
+# dat_sum_2 <-
+#   dat_sum_2 %>%
+#   rename(assignment = factor_1,
+#          outcome = factor_2)
+
+
+write_csv(dat_sum_2, "example/dat_sum_2.csv")
+
+
+
+
+ggplot(dat_sum, aes(x = factor_1, y = factor_2, color = factor_3, size = n_studies)) +
+  #geom_jitter(alpha = 0.5, position = position_dodge(width = 0.5)) + 
+  geom_point(alpha = 0.5, aes(group = factor_3), 
+             position = position_dodge(width= 0.5)) + 
+  scale_size_identity() +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
+  scale_y_discrete(labels = function(x) str_wrap(x, width = 10)) +
+  geom_text(aes(label = as.character(n_studies), group = factor_3), 
+            size = 2.5, color = "black",
+            position = position_dodge(width = .5)) +
+  labs(caption = "Number of studies per combination of factors are overlaid.") +
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+
+dat_sum <-
+  dat %>%
+  group_by(factor_1, factor_2, factor_3) %>%
+  group_modify(~ tidy_meta(.x)) %>%
+  ungroup()
