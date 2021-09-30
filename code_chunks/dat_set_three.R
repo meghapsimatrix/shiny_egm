15d6894c2034b1c064d3ebdfd326ce15f2847f6b
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2021-09-30 16:44:30 -0500

    typo

--- code_chunks/dat_set_three.R
+++ code_chunks/dat_set_three.R
@@ -5,7 +5,7 @@ source('tidy_meta.R')
 
 summary_dat <- 
   dat %>%
-  group_by(factor_1, factor_2) %>%
+  group_by(factor_1, factor_2, factor_3) %>%
   group_modify(~ tidy_meta(.x, 
                            model = "{user_mod}", 
                            rho_val = {user_rho})) %>%
