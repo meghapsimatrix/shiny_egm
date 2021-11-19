c30358f19afdc496edb1bf8c3816dcf2ecb7dcaa
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2021-11-19 10:20:03 -0600

    only the  ones needed

--- extra/continuous_color.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-
-dat <- read_csv("datasets/dat_sum_comma_3.csv") %>%
-  rename(estimate = beta)
-
-
-make_egm_plot(data = dat, factors_n = "three", es = FALSE, xlabel = "", ylabel = "", shapelabel = "")
